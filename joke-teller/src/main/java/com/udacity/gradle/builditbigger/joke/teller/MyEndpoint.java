40e675adbfebd07d229c8f0e2bef574cc9c5b202
chipset <chipset95@yahoo.co.in>
2016-01-17 15:34:29 +0530

    app engine added

---
+++ joke-teller/src/main/java/com/udacity/gradle/builditbigger/joke/teller/MyEndpoint.java
@@ -0,0 +1,35 @@
+/*
+   For step-by-step instructions on connecting your Android application to this backend module,
+   see "App Engine Java Endpoints Module" template documentation at
+   https://github.com/GoogleCloudPlatform/gradle-appengine-templates/tree/master/HelloEndpoints
+*/
+
+package com.udacity.gradle.builditbigger.joke.teller;
+
+import com.google.api.server.spi.config.Api;
+import com.google.api.server.spi.config.ApiMethod;
+import com.google.api.server.spi.config.ApiNamespace;
+import com.udacity.gradle.builditbigger.joker.java.JokeMaker;
+
+/**
+ * An endpoint class we are exposing
+ */
+@Api(
+        name = "jokeApi",
+        version = "v1",
+        namespace = @ApiNamespace(
+                ownerDomain = "teller.joke.builditbigger.gradle.udacity.com",
+                ownerName = "teller.joke.builditbigger.gradle.udacity.com",
+                packagePath = ""
+        )
+)
+public class MyEndpoint {
+
+    @ApiMethod(name = "jokeMe")
+    public MyBean jokeMe() {
+        MyBean response = new MyBean();
+        response.setData(JokeMaker.getAJoke());
+        return response;
+    }
+
+}
--- jokes-java/src/main/java/com/udacity/gradle/builditbigger/joker/java/JokeMaker.java
+++ jokes-java/src/main/java/com/udacity/gradle/builditbigger/joker/java/JokeMaker.java
@@ -1,8 +1,15 @@
 package com.udacity.gradle.builditbigger.joker.java;
 
+import java.util.Random;
+
 public class JokeMaker {
 
-    public String getAJoke() {
-        return "Why did the chicken cross the road?\n\nTo get to the other side";
+    private static String jokes[] = {"Why did the chicken cross the road?\n\nTo get to the other side",
+            "The only time incorrectly isn’t spelled incorrectly is when it’s spelled incorrectly",
+            "I used to think the brain was the most important organ. Then I thought, look what’s telling me that",
+            "Today a man knocked on my door and asked for a small donation towards the local swimming pool. I gave him a glass of water"};
+
+    public static String getAJoke() {
+        return jokes[new Random().nextInt(jokes.length + 1)];
     }
 }
